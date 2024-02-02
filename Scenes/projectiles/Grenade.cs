c277be8f7229adfee7c25602853abb047c6c5e0b
Brian <brianchan98@hotmail.com>
2024-02-02 15:11:45 -0500

    created fan animation and a really cool grenade animation!

--- Scenes/projectiles/Grenade.cs
+++ Scenes/projectiles/Grenade.cs
@@ -3,5 +3,21 @@ using System;
 
 public partial class Grenade : RigidBody2D
 {
+    AnimationPlayer animation;
+    Sprite2D explode;
+
     public const int SPEED = 750;
+
+    public override void _Ready()
+    {
+        animation = GetNode<AnimationPlayer>("AnimationPlayer");
+        explode = GetNode<Sprite2D>("Explosion");
+        explode.Visible = false;
+    }
+
+    public void explosion()
+    {
+        explode.Visible = true;
+        animation.Play("Explosion");
+    }
 }
